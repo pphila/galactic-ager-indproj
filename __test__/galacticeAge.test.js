5f14cea4f30090a3e475b44bc5b96291571d6d26
pphila <efl2ice89@gmail.com>
2022-10-07 21:04:49 -0700

    start red phase for galactic age

---
+++ __test__/galacticeAge.test.js
@@ -0,0 +1,8 @@
+import { EarthAge } from './../src/js/galacticAge.js';
+
+describe('EarthAge', () => {
+
+  test('should correctly return earth age object with life expetancy factors', () => {
+    
+  })
+})
