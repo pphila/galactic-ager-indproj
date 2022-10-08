ce8e5f34016cafb55293b6b89e89506a74b4620f
pphila <efl2ice89@gmail.com>
2022-10-08 11:07:35 -0700

    add red phase to Mars test

--- __test__/mars.test.js
+++ __test__/mars.test.js
@@ -1,1 +1,13 @@
 //Fourth planet
+
+import Earth from './../src/js/galacticAge.js';
+import Mars from './../src/js/mars.js';
+
+describe('Mars', () => {
+  
+  test('should correctly return an number for age in Mars years', () => {
+    let marsInpt = new Mars(20);
+    marsInpt.marsCalc();
+    expect(marsInpt.marsAge).toEqual(83);
+  });
+});
