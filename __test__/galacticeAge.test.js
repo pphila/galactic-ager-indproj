303f17728ef566f77f27763618a316eb9f24734d
pphila <efl2ice89@gmail.com>
2022-10-08 12:25:09 -0700

    add fix to bad pass for second Earth obj test

--- __test__/galacticeAge.test.js
+++ __test__/galacticeAge.test.js
@@ -10,11 +10,13 @@ describe('Earth', () => {
 
   test('should return a life expectancy for biological male choice', () => {
     let earthAge = new Earth(20, "male");
+    earthAge.lifeExptCalc();
     expect(earthAge.ageExpect).toEqual(79); //accoding to recent US census is about 79
   })
 
   test('should return a life expectancy for biological female choice', () => {
     let earthAge = new Earth(20, "female");
+    earthAge.lifeExptCalc();
     expect(earthAge.ageExpect).toEqual(81); //US femail census is abour 81
   })
 });
--- src/js/galacticAge.js
+++ src/js/galacticAge.js
@@ -2,6 +2,12 @@ export default class Earth {
   constructor(age, gender) {
     this.age = age;
     this.gender = gender;
-    this.ageExpect = 79;
+    this.ageExpect = this.lifeExptCalc();
+  }
+
+  lifeExptCalc(age) {
+    if (this.gender === "male") {
+      age = 79;
+    } return age; 
   }
 }
