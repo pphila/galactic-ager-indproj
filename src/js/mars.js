573620de2de6e3eb15094c9804375dd7bdaa7d20
pphila <efl2ice89@gmail.com>
2022-10-08 16:20:44 -0700

    add logic to pass second Mars test

--- src/js/mars.js
+++ src/js/mars.js
@@ -2,9 +2,14 @@ export default class Mars {
   constructor(earthAge) {
     this.earthAge = earthAge;
     this.marsAge = 0;
+    this.marsQtn = 1.88;
   }
 
   marsCalc() {
     this.marsAge = Math.round(this.earthAge / 1.88);
+  }
+
+  differenceCalc(Earth) {
+    this.ageRemainder = Math.round(Math.abs(Earth - this.marsAge))
   }
 }
