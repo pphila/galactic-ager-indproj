53f90c9c579bce4c8212296289391bdeee554469
pphila <efl2ice89@gmail.com>
2022-10-08 10:56:03 -0700

    add red phase to first Venus test

--- __test__/venus.test.js
+++ __test__/venus.test.js
@@ -1,1 +1,13 @@
 // Second planet
+
+import Earth from './../src/js/galacticAge.js';
+import Venus from './../src/js/venus';
+
+describe('Venus', () => {
+
+  test('should correctly return a number for age in Venus years', () => {
+    let venusInput = new Venus(20);
+    venusInput.venusCalc();
+    expect(venusInput.venusAge).toEqual(32);
+  });
+});
