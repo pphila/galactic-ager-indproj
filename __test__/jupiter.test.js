d5d90a4ad7b473a901ffec49286cdd2541bbb7fe
pphila <efl2ice89@gmail.com>
2022-10-08 16:24:36 -0700

    add red phase to second Jupiter test

--- __test__/jupiter.test.js
+++ __test__/jupiter.test.js
@@ -9,5 +9,14 @@ describe('Jupiter', () => {
     let jupiterInpt = new Jupiter(20);
     jupiterInpt.jupiterCalc();
     expect(jupiterInpt.jupiterAge).toEqual(2)
-  })
-})
+  });
+
+  test('should return age expectancy and difference according to Jupiter age', () =>  {
+    let jupiterInpt = new Mars(20);
+    jupiterInpt.jupiterCalc();
+    let earthAge = new Earth(20, "female"); //fLE = 81
+    let jupiterLE = earthAge.lifeExpectancy(jupterInpt. jupiterQtn); //LE = life expectancy, jupiter qtn = 11.86
+    jupuiterInpt.differenceCalc(jupiterLE);
+    expect(jupiterInpt.ageRemainder).toEqual(5)
+  });
+});
