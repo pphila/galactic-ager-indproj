e032b77e7c716f4ea66ad8ab4b58c9651e2c1237
pphila <efl2ice89@gmail.com>
2022-10-08 11:42:43 -0700

    add red phase to second test for Earth object

--- __test__/galacticeAge.test.js
+++ __test__/galacticeAge.test.js
@@ -1,10 +1,15 @@
 import Earth from './../src/js/galacticAge.js';
 
-describe('EarthAge', () => {
+describe('Earth', () => {
 
   test('should correctly return earth age object with life expetancy factors', () => {
-    const earth = new Earth(20, "male");
+    let earth = new Earth(20, "male");
     expect(earth.age).toEqual(20);
     expect(earth.gender).toMatch('male');
   });
+
+  test('should return a life expectancy for biological male choice', () => {
+    let earth = new Earth(20, "male");
+    expect(earth.ageExpect).toEqual(79); //accoding to recent US census is about 79
+  })
 });
