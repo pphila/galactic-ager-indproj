e04a0ed5295e54c8557cd2bfbcb3e6fec6755ca0
pphila <efl2ice89@gmail.com>
2022-10-08 16:16:32 -0700

    add red phase to second Mars test

--- __test__/mars.test.js
+++ __test__/mars.test.js
@@ -10,4 +10,13 @@ describe('Mars', () => {
     marsInpt.marsCalc();
     expect(marsInpt.marsAge).toEqual(11);
   });
+
+  test('should return age expectancy and difference according to Mars age', () =>  {
+    let marsInpt = new Mars(20);
+    marsInpt.marsCalc();
+    let earthAge = new Earth(20, "female"); //fLE = 81
+    let marsLE = earthAge.lifeExpectancy(marsInpt.marsQtn); //LE = life expectancy, mars qtn = 1.88
+    marsInpt.differenceCalc(marsLE);
+    expect(marsInpt.ageRemainder).toEqual(32)
+  })
 });
