2429bee67124023e4f975871862f50e69a49ed88
pphila <efl2ice89@gmail.com>
2022-10-08 16:09:09 -0700

    add red phase for 2nd Venus test

--- __test__/venus.test.js
+++ __test__/venus.test.js
@@ -10,4 +10,13 @@ describe('Venus', () => {
     venusInput.venusCalc();
     expect(venusInput.venusAge).toEqual(32);
   });
+
+  test('should return age expectancy and difference according to Venus age', () =>  {
+    let venusInpt = new Venus(20);
+    venusInpt.venusCalc();
+    let earthAge = new Earth(20, "male");
+    let venusLE = earthAge.lifeExpectancy(venusInpt.venusQtn); //venus quotient = .62
+    venusInpt.differenceCalc(venusLE);
+    expect(venusInpt.ageRemainder).toEqual(95)
+  })
 });
