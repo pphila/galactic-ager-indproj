de1e686b9fc03fc539e041cd08efd22db79676fa
pphila <efl2ice89@gmail.com>
2022-10-08 10:22:49 -0700

    add red phase test 1 to mercury.js

--- __test__/mercury.test.js
+++ __test__/mercury.test.js
@@ -1,1 +1,14 @@
-//First planet in the solar sytem
+//First planet in the solar system
+
+import { TestScheduler } from 'jest';
+import Earth from './../src/js/galacticAge.js';
+import Mercury from './../src/js/mercury.js';
+
+describe('Mercury', () => {
+  
+  test('should correctly return an number for age in Mercury years', () => {
+    let mercuryInpt = new Mecury(20);
+    mercuryInpt.mercuryAgeCalc();
+    expect(mercuryAgeCalc).toEqual(83);
+  })
+})
