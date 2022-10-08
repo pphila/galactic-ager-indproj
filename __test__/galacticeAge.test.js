867e29c5b1ae032e50c5b4935d0d41cec391c53f
pphila <efl2ice89@gmail.com>
2022-10-07 21:33:27 -0700

    add good fail to first test

--- __test__/galacticeAge.test.js
+++ __test__/galacticeAge.test.js
@@ -3,6 +3,8 @@ import { EarthAge } from './../src/js/galacticAge.js';
 describe('EarthAge', () => {
 
   test('should correctly return earth age object with life expetancy factors', () => {
-    
-  })
-})
+    const earthAge = new EarthAge(20, "male");
+    expect(earthAge.age).toEqual(20);
+    expect(earthAge.gender).toMatch('male');
+  });
+});
