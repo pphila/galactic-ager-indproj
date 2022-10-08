3ab880e7a8b260534cb975fe3025c7d78e761c32
pphila <efl2ice89@gmail.com>
2022-10-08 11:26:53 -0700

    add red phase to Jupiter test

--- __test__/jupiter.test.js
+++ __test__/jupiter.test.js
@@ -1,1 +1,13 @@
 //Fifth planet
+
+import { TestScheduler } from 'jest';
+import Earth from './../src/js/galacticAge.js';
+import Jupiter from './../src/js/jupiter.js';
+
+describe('Jupiter', () => {
+  test('should correctly return a number for age in Jupiter years', () => {
+    let jupiterInpt = new Jupiter(20);
+    jupiterInpt.jupiterCalc();
+    expect(jupiterInpt.jupiterAge).toEqual(2)
+  })
+})
